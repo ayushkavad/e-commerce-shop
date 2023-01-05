1a4606129cb26d7e788d1321c0dee2835531a029
Ayush Kavad <ayushkavad@gmail.com>
2023-01-05 22:51:49 +0530

    checkout item

--- src/component/category-preview/category-preview.styles.jsx
+++ src/component/category-preview/category-preview.styles.jsx
@@ -22,21 +22,3 @@ export const Preview = styled.div`
   grid-template-columns: repeat(4, 1fr);
   column-gap: 20px;
 `;
-
-// .category-preview-container {
-// display: flex;
-// flex-direction: column;
-// margin-bottom: 30px;
-
-//   .title {
-// font-size: 28px;
-// margin-bottom: 25px;
-// cursor: pointer;
-//   }
-
-//   .preview {
-// display: grid;
-// grid-template-columns: repeat(4, 1fr);
-// column-gap: 20px;
-//   }
-// }
--- src/component/checkout-item/checkout-item.component.jsx
+++ src/component/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,14 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-import './checkout-item.styles.scss';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  Image,
+  Allstyle,
+  Arrow,
+  Value,
+  RemoveButton,
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, quantity, price } = cartItem;
@@ -20,26 +28,20 @@ const CheckoutItem = ({ cartItem }) => {
   };
 
   return (
-    <div className="checkout-item-container">
-      <div className="image-container">
-        <img src={imageUrl} alt={`${name}`} />
-      </div>
-      <span className="name">{name}</span>
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <Image src={imageUrl} alt={`${name}`} />
+      </ImageContainer>
+      <Allstyle>{name}</Allstyle>
 
-      <span className="quantity">
-        <div className="arrow" onClick={decrementCartItem}>
-          &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={incrementCartItem}>
-          &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={clearCartItem}>
-        &#10005;
-      </div>
-    </div>
+      <Allstyle>
+        <Arrow onClick={decrementCartItem}>&#10094;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={incrementCartItem}>&#10095;</Arrow>
+      </Allstyle>
+      <Allstyle>{price}</Allstyle>
+      <RemoveButton onClick={clearCartItem}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
 };
 
