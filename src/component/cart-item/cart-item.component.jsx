220b273ad10473a0e632900be6e6b458f2b0ba8d
Ayush Kavad <ayushkavad@gmail.com>
2023-01-05 20:55:09 +0530

    cart-item component

--- src/component/cart-item/cart-item.component.jsx
+++ src/component/cart-item/cart-item.component.jsx
@@ -1,17 +1,23 @@
-import './cart-item.styles.scss';
+import {
+  CartItemContainer,
+  Image,
+  ItemDetails,
+  Name,
+  Price,
+} from './cart-item.styles';
 
 const CartItem = ({ cartItem }) => {
   const { name, quantity, imageUrl, price } = cartItem;
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={name} />
-      <div className="item-details">
-        <h2 className="name">{name}</h2>
-        <span className="price">
+    <CartItemContainer>
+      <Image src={imageUrl} alt={name} />
+      <ItemDetails>
+        <Name>{name}</Name>
+        <Price>
           {quantity} x ${price}
-        </span>
-      </div>
-    </div>
+        </Price>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
